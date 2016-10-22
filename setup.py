67ea68b421cabbed790e72f9b145af57d9cf735d
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-10-22 19:44:52 +0100

    Bump version to v0.2.3
    Fixes a bug.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name="django-onfido",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=[
         'django>=1.8',
