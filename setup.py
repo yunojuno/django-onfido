9d753bda9fe6f9aac053e5383616289f81cda062
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-10-24 23:18:32 +0100

    Bump version to v0.4.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name="django-onfido",
-    version="0.2.3",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=[
         'django>=1.8',
