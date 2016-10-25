6534c6c026cdf0fa7b76cd46655adb8b5cee006c
Hugo Rodger-Brown <hugo@rodger-brown.com>
2016-10-25 10:45:45 +0100

    Improve admin views
    Better sorting / filtering / searching.
    
     Search applicants, checks, reports by onfido_id
     Search applicants, checks, reports by user first / last name
     Filter checks, reports by result
     Display applicant user.get_full_name() instead of username

--- onfido/admin.py
+++ onfido/admin.py
@@ -34,56 +34,106 @@ class RawMixin(object):
         )
         html = pretty.replace(" ", "&nbsp;").replace("\n", "<br>")
         return mark_safe("<code>{}</code>".format(html))
+
     _raw.short_description = _("Raw (from API)")
 
 
-class ApplicantAdmin(RawMixin, admin.ModelAdmin):
+class UserMixin(object):
+
+    """Admin mixin used to add _user function."""
+
+    def _user(self, obj):
+        """
+        Return user's real name.
+
+        It's impossible to create an Applicant in the Onfido API
+        with a blank first/last name, so assuming that get_full_name
+        will not be blank.
+
+        """
+        return obj.user.get_full_name().title()
+
+    _user.short_description = 'User'
+
+
+class ApplicantAdmin(RawMixin, UserMixin, admin.ModelAdmin):
 
     """Admin model for Applicant objects."""
 
-    list_display = ('user', 'created_at')
+    list_display = (
+        'onfido_id', '_user', 'created_at'
+    )
     list_filter = ('created_at',)
-    ordering = ('user__first_name', 'user__last_name')
-    readonly_fields = ('onfido_id', 'user', 'created_at', '_raw')
+    ordering = (
+        'user__last_name', 'user__first_name', 'user__username'
+    )
+    readonly_fields = (
+        'onfido_id', 'user', 'created_at', '_raw'
+    )
+    search_fields = (
+        'onfido_id', 'user__first_name', 'user__last_name'
+    )
     raw_id_fields = ('user',)
     exclude = ('raw',)
 
 admin.site.register(Applicant, ApplicantAdmin)
 
 
-class CheckAdmin(RawMixin, admin.ModelAdmin):
+class CheckAdmin(RawMixin, UserMixin, admin.ModelAdmin):
 
     """Admin model for Check objects."""
 
     list_display = (
-        'applicant', 'check_type', 'status',
+        'onfido_id', '_user', 'check_type', 'status',
         'result', 'created_at', 'updated_at'
     )
     readonly_fields = (
         'onfido_id', 'user', 'created_at', 'applicant', 'check_type',
         'status', 'result', 'updated_at', '_raw'
     )
-    list_filter = ('check_type', 'created_at', 'status')
-    raw_id_fields = ('applicant', 'user')
+    search_fields = (
+        'onfido_id', 'user__first_name', 'user__last_name'
+    )
+    list_filter = (
+        'created_at', 'updated_at', 'check_type',
+        'status', 'result'
+    )
+    ordering = (
+        'user__first_name', 'user__last_name'
+    )
+    raw_id_fields = (
+        'applicant', 'user'
+    )
     exclude = ('raw',)
 
 admin.site.register(Check, CheckAdmin)
 
 
-class ReportAdmin(RawMixin, admin.ModelAdmin):
+class ReportAdmin(RawMixin, UserMixin, admin.ModelAdmin):
 
     """Admin model for Report objects."""
 
     list_display = (
-        'onfido_check', 'report_type', 'status',
-        'result', 'created_at', 'updated_at'
+        'onfido_id', '_user', 'report_type',
+        'status', 'result', 'created_at', 'updated_at'
+    )
+    ordering = (
+        'user__first_name', 'user__last_name'
     )
     readonly_fields = (
         'onfido_id', 'user', 'onfido_check', 'report_type',
         'status', 'result', 'created_at', 'updated_at', '_raw'
     )
-    list_filter = ('created_at', 'report_type', 'status')
-    raw_id_fields = ('onfido_check', 'user')
+    search_fields = (
+        'onfido_id', 'onfido_check__onfido_id',
+        'user__first_name', 'user__last_name'
+    )
+    list_filter = (
+        'created_at', 'updated_at', 'report_type', 'status', 'result'
+    )
+    raw_id_fields = (
+        'onfido_check', 'user'
+    )
     exclude = ('raw',)
 
 admin.site.register(Report, ReportAdmin)
@@ -94,14 +144,17 @@ class EventAdmin(RawMixin, admin.ModelAdmin):
     """Admin model for Event objects."""
 
     list_display = (
-        'resource_id', 'resource_type', 'action', 'status', 'completed_at'
+        'resource_id', 'resource_type', 'action',
+        'status', 'completed_at'
     )
     list_filter = (
         'action', 'resource_type', 'completed_at'
     )
     readonly_fields = (
-        'resource_id', 'resource_type', 'action', 'status', 'completed_at', '_raw'
+        'resource_id', 'resource_type', 'action',
+        'status', 'completed_at', '_raw'
     )
+    search_fields = ('resource_id',)
     exclude = ('raw',)
 
 admin.site.register(Event, EventAdmin)
