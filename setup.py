587d7653b36eb7a6fcb505621d39f3db07980707
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-11-14 20:30:42 +0000

    Bump version to v0.7.0
    Includes database migrations.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name="django-onfido",
-    version="0.6.1",
+    version="0.7.0",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     include_package_data=True,
