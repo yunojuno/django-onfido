ab0a3fbb7be6c12afb08f11a4fb4a79703ad4ca9
Hugo Rodger-Brown <hugo@rodger-brown.com>
2016-12-24 10:26:49 +0000

    Fix missing migration, bump version to v0.8.4

--- onfido/migrations/0011_add_indexes_to_status_fields.py
+++ onfido/migrations/0011_add_indexes_to_status_fields.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('onfido', '0010_convert_raw_fields_to_jsonb'),
+        ('onfido', '0009_add_is_clear_field'),
     ]
 
     operations = [
