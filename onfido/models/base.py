41d536a97583faffc0d778a7ea11ca196e728491
culshaw <culshaw@gmail.com>
2021-05-05 15:44:47 +0100

    Replace `is_clear` database field with computed is_clear property. (#29)
    Co-authored-by: Hugo Rodger-Brown <hugorodgerbrown@users.noreply.github.com>

--- onfido/models/base.py
+++ onfido/models/base.py
@@ -153,14 +153,6 @@ class BaseStatusModel(BaseModel):
         null=True,
         help_text=_("The timestamp of the most recent status change (from API)."),
     )
-    is_clear = models.BooleanField(
-        default=None,
-        null=True,
-        blank=True,
-        help_text=_(
-            "True if the check / report is 'clear' (via API or manual override)."
-        ),
-    )
 
     class Meta:
         abstract = True
@@ -258,8 +250,6 @@ class BaseStatusModel(BaseModel):
         super().parse(raw_json)
         self.result = self.raw["result"]
         self.status = self.raw["status"]
-        if self.result == self.Result.CLEAR:
-            self.is_clear = True
         return self
 
     def mark_as_clear(self, user: settings.AUTH_USER_MODEL) -> Event:
@@ -285,7 +275,15 @@ class BaseStatusModel(BaseModel):
         Returns the object itself, updated and saved.
 
         """
-        self._override_event(user).save()
-        self.is_clear = True
+        event = self._override_event(user)
+        event.raw["payload"]["result"] = self.Result.CLEAR
+        event.save()
+
+        self.result = self.Result.CLEAR
         self.save()
         return self
+
+    @property
+    def is_clear(self) -> bool:
+        """Return True/False to whether a check is successful and clear."""
+        return self.result == self.Result.CLEAR
